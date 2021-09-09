576a3160f2b9eb96f37b2c9dc309185b622350b9
Kimberlly Amaral <kimberllys.amaral@gmail.com>
2021-09-09 14:27:11 -0300

    script basico da idade adicionado, erros corrigidos

--- aulaEx012/ex015/script.js
+++ aulaEx012/ex015/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var resultado = document.querySelector('div#resultado')
+
+// se o ano inserido for igual a 0 ou se for maior que o ano atual = mensagem de erro
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex');
+        var idade = ano - Number(fano.value)
+        resultado.innerHTML = `Idade calculada: ${idade}`
+
+    }
+}
