69b021ad924bb6d06ea276cea045183ac49441d4
Kimberlly Amaral <kimberllys.amaral@gmail.com>
2021-09-10 12:04:20 -0300

    exercicio ex015 finalizado

--- aulaEx012/ex015/script.js
+++ aulaEx012/ex015/script.js
@@ -11,6 +11,43 @@ function verificar() {
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fano.value)
         resultado.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document. createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'homembebe.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'homemjovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'homemadulto.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'homemidoso.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // criança
+                img.setAttribute('src', 'mulherbebe.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'mulherjovem.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'mulheradulta.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'mulheridosa.png')
+            }
+        }
+        resultado.style.textAlign = 'center'
+        resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        resultado.appendChild(img)
 
     }
 }
